cdb0d0a3d5378fa6b1b688e8e913054cdf73895e
akmalghandi20 <gandolkikok@gmail.com>
2025-01-08 20:47:08 +0700

    "akmal : pembuatan user button dan dan membuat file dialog.tsx"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
+// import Image from "next/image";
+// import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="p-4">
-        <Button variant={"destructive"} size={"lg"}>
-        Click
-        </Button>
+        <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
