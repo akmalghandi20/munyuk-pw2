6f2271bf3e3023685a5427148a35cb7a64c2fb37
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-09 00:34:49 +0700

    dayanu: mendefinisikan formSchema

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,7 +1,16 @@
 'use client'
+import * as z from 'zod'
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import Modal from "@/components/ui/modal";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
+
+
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
