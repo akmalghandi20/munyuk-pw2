9fd980eca4bea2b1185d2f67bc3d248237c2cc21
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-09 00:16:44 +0700

    dayanu: menampilkan popup untuk user yang belum punya store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,13 +4,24 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const StartPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen()
+    }
+
+  },[isOpen, onOpen])
+
+
   return (
     <div className="p-4">
-        <Modal title="Test Title" description="Test Desc" isOpen={true} onClose={() => {}} >
-          Children
-        </Modal>
+        Root Page
     </div>
   );
 }
