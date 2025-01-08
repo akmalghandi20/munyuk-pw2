d26574b4e76d0fb78860e19fdfe2ab9e5d590481
akmalghandi20 <gandolkikok@gmail.com>
2025-01-08 20:11:44 +0700

    "akmal : membuat layout untuk tampilan sign in dan sign up"

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
     children: React.ReactNode
 }){
     return(
-        <div>
+        <div className="flex items-center justify-center h-full">
             {children}
         </div>
     )
