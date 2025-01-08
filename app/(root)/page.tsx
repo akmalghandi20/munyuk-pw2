83b0506914f6199a679b3a1afc6874cb13b31ebe
akmalghandi20 <gandolkikok@gmail.com>
2025-01-08 21:06:28 +0700

    "akmal : membuat komponen modal dan file modal.tsx pada folder components/ui"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 // import Image from "next/image";
 // import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-export default function Home() {
+import Modal from "@/components/ui/modal";
+// import { UserButton } from "@clerk/nextjs";
+export default function StartPage() {
   return (
     <div className="p-4">
-        <UserButton afterSignOutUrl="/"/>
+        <Modal title="Test Title" description="Test Desc" isOpen={true} onClose={() => {}} >
+          Children
+        </Modal>
     </div>
   );
 }
