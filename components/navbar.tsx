890e1dffc39b4dcb599f54401ba1b04b8f707932
akmalghandi20 <gandolkikok@gmail.com>
2025-01-20 22:12:51 +0700

    akmal : memperbaiki bar navigasi agar bisa menampilkan nama toko dan bisa membuat toko di bar navigasinya

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,28 @@
 import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
 import StoreSwitcher from "./store-switcher";
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = await auth();
+
+    if(!userId){
+        redirect("/sign-in");
+    }
+
+    const stores = await db.store.findMany({
+        where:{
+            userId: userId
+        }
+    });
+
     return ( 
         <div className="border-b">
              <div className="flex h-16 items-center px-4 ">
                 
-                <StoreSwitcher/>
+                <StoreSwitcher items={stores} />
 
                 <MainNav className="mx-6"/>
 
