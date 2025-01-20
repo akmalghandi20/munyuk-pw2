859c6968eadf943bc5ee968a69beef58576ac26f
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 18:29:33 +0700

    dayanu: mendefinisikan StoreModal, params, router dan currentStore beserta pengkondisiannya

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -2,6 +2,8 @@
 
 import { Store } from "@prisma/client";
 import { PopoverTrigger } from "./ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
 
 type PopOverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -13,6 +15,18 @@ const StoreSwitcher = ({
     className,
     items = []
 }: StoreSwitcherProps ) => {
+
+    const StoreModal = useStoreModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const formattedItems = items.map((item) => ({
+        label: item.name,
+        value: item.id
+    }))
+
+    const currentStore = formattedItems.find((item) => item.value === params.storeId)
+
     return ( 
         <div>
             Store Switcher New
