e77df6a3a63d4be4fc68039169bdda603827f3ae
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 16:47:04 +0700

    dayanu: membuat komponen navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,16 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
     return ( 
         <div className="border-b">
              <div className="flex h-16 items-center px-4 ">
-
+                <div> Store Switcher</div>
+                <div> Main Nav</div>
+                <div> <UserButton/> </div>
+                
              </div>
              
-             </div>
+        </div>
      );
 }
  
