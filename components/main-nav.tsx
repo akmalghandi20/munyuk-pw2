168ed013405dfed3c8082ef22317889390cfb54c
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 17:15:06 +0700

    dayanu: menambahkan class untuk css dan fungsi cn pada Main nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { cn } from "@/lib/utils"
+
 export function MainNav({
     classname,
     ...props
 }: React.HTMLAttributes<HTMLElement>) {
+
+    
     return (
-        <nav>
+        <nav className={cn(
+            "flex items-center space-x-4 lg:space-x-6", 
+            classname
+        )}>
             Ini Main Nav
         </nav>
     )
