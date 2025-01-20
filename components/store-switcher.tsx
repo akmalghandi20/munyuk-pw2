f0bef483600630073558d9cdcea790e7f3605ce1
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 21:16:14 +0700

    dayanu: memmperbaiki error pada auth di navbar.tsx

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -53,7 +53,7 @@ const StoreSwitcher = ({
                 className={cn("w-[200px] justify-between",className)}
                 >
                     <StoreIcon className="mr-2 h-4 w-4"/>
-                    {currentStore?.label || "Pilih Toko"}
+                    {currentStore?.label}
                     <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
                 </Button>
             </PopoverTrigger>
