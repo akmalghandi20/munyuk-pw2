88af3e9f9c10299403bf3925905ff9a2fb037b80
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 17:24:49 +0700

    dayanu: Membuat array routes yang berisi objek rute, di mana active bernilai true jika pathname cocok dengan URL rute tersebut

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,17 +1,28 @@
 'use client'
 
 import { cn } from "@/lib/utils"
+import { useParams, usePathname } from "next/navigation"
 
 export function MainNav({
-    classname,
+    className,
     ...props
 }: React.HTMLAttributes<HTMLElement>) {
 
+    const pathname = usePathname();
+    const params = useParams();
+
+    const routes = [
+        {
+            href: `/${params.storeId}/settings`,
+            label: 'settings',
+            active: pathname === `/${params.storeId}/settings`,
+        }
+    ]
     
     return (
         <nav className={cn(
             "flex items-center space-x-4 lg:space-x-6", 
-            classname
+            className
         )}>
             Ini Main Nav
         </nav>
