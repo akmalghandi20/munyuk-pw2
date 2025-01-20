3f16019015e42a8c3f70a70e4f6435cd190f64cc
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 18:21:55 +0700

    dayanu: mendefinisikan komponen StoreSwitcher

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,4 +1,18 @@
-const StoreSwitcher = () => {
+'use client'
+
+import { Store } from "@prisma/client";
+import { PopoverTrigger } from "./ui/popover";
+
+type PopOverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
+
+interface StoreSwitcherProps extends PopOverTriggerProps {
+    items: Store[];
+}
+
+const StoreSwitcher = ({
+    className,
+    items = []
+}: StoreSwitcherProps ) => {
     return ( 
         <div>
             Store Switcher New
