3b38590028ee0aa644ffdcc3169ccc950b6e918f
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-20 16:43:05 +0700

    dayanu: melayout tombol navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,11 @@
 const Navbar = () => {
     return ( 
-        <div className="border-b"> Ini tombol navbar</div>
+        <div className="border-b">
+             <div className="flex h-16 items-center px-4 ">
+
+             </div>
+             
+             </div>
      );
 }
  
