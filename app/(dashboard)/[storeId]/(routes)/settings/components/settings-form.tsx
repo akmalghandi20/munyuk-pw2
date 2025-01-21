a96db68215a7c9710768e26251ca6d47604c9b75
RastomiOF <raspamungkas07@gmail.com>
2025-01-21 11:34:37 +0700

    rastomi: menambahkan validasi form dengan zod dan react-hook-form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,21 +1,55 @@
 'use client'
 
+import * as z from 'zod'
+import { useState } from 'react'
+
+import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
 import { Store } from "@prisma/client"
+import { Trash } from "lucide-react"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 interface SettingPageProps {
-    initialData: Store
+    initialData: Store;
 }
 
+const formSchema = z.object({
+    name: z.string().min(1)
+})
+
+type SettingsFormValue = z.infer<typeof formSchema>
+
 export const SettingsForm: React.FC<SettingPageProps> = (
-    initialData
+    {initialData}
 ) => {
+
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    
+
+    const form = useForm<SettingsFormValue>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+
     return(
+        <>
         <div className="flex items-center justify-between">
             <Heading
                 title="Settings"
                 description="Atur Toko"
             />
+            <Button
+            variant="destructive"
+            size="sm"
+            onClick={() =>{} }
+            >
+                <Trash className="h-4 w-4"/>
+            </Button>
         </div>
+        <Separator />
+        </>
     )
 }
