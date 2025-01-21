127529989596e3f4b82d76d498cfaac304c57d8f
RastomiOF <raspamungkas07@gmail.com>
2025-01-21 10:15:42 +0700

    rastomi: Tambahkan autentikasi pengguna dan validasi toko berdasarkan ID pengguna dan ID toko

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,4 +1,34 @@
-const SettingPage = () => {
+import db from "@/lib/db";
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+
+interface SettingPageProps {
+    params: {
+        storeId: string;
+    }
+}
+
+const SettingPage: React.FC<SettingPageProps> = async({
+    params
+}) => {
+
+    const { userId } = await auth();
+
+    if(!userId){
+        redirect('/sign-in')
+    }
+
+    const store = await db.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    })
+
+    if(!store){
+        redirect('/')
+    }
+
     return (
         <div>
             ini page setting
