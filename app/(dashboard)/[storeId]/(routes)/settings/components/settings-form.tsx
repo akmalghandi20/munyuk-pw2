042b816e2ee29982effa0c7f3b626b7bf2ceb0c8
akmalghandi20 <gandolkikok@gmail.com>
2025-01-21 15:44:40 +0700

    akmal : Membuat fungsi untuk update data dan hapus data

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,7 +10,7 @@ import { Store } from "@prisma/client"
 import { Trash } from "lucide-react"
 import { Form, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 
 interface SettingPageProps {
@@ -27,9 +27,7 @@ export const SettingsForm: React.FC<SettingPageProps> = (
     {initialData}
 ) => {
 
-    const [open, setOpen] = useState(false)
     const [loading, setLoading] = useState(false)
-
 
     const form = useForm<SettingsFormValue>({
         resolver: zodResolver(formSchema),
@@ -38,6 +36,7 @@ export const SettingsForm: React.FC<SettingPageProps> = (
 
     const onSubmit = async(data: SettingsFormValue) => {
         console.log(data)
+        setLoading(true) 
     }
 
     return(
@@ -48,32 +47,38 @@ export const SettingsForm: React.FC<SettingPageProps> = (
                 description="Atur Toko"
             />
             <Button
+            disabled={loading}
             variant="destructive"
             size="sm"
-            onClick={() =>{} }
+            onClick={() => setOpen(true)}
             >
                 <Trash className="h-4 w-4"/>
             </Button>
         </div>
         <Separator />
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-                <div className="grid grid-cols-3 gap-8">
-                    <FormField 
-                        control={form.control}
-                        name="name"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Nama</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Nama Toko" disabled={loading} />
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
-                </div>
-            </form>
-        </Form>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+            <div className="grid grid-cols-3 gap-8">
+                <FormField 
+                    control={form.control}
+                    name="name"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Nama</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Nama Toko Perlu Diinputkan" disabled={loading} value={field.value} onChange={field.onChange} /> 
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <Button
+            disabled={loading}
+            type='submit'
+            >
+                Simpan
+            </Button>
+        </form>
         </>
     )
 }
