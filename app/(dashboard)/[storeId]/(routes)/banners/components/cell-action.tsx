539fc7b29feecd1f32f8aa6d3f6c1f84bdff156b
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-25 22:16:15 +0700

    dayanu: merubah tombol pada tabel menjadi dropdownmenu action, lalu menambahkan tombol untuk copu, delete, dan update

--- app/(dashboard)/[storeId]/(routes)/banners/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/components/cell-action.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { BannerColumn } from "./columns"
 import { Button } from "@/components/ui/button"
+import { Copy, Delete, Edit, MoreHorizontal } from "lucide-react"
 
 interface CellActionProps {
     data: BannerColumn
@@ -15,12 +16,30 @@ export const CellAction: React.FC<CellActionProps> = ({
         <div>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button>
-                        <span>
+                    <Button variant="ghost">
+                        <span className="sr-only">
                             Open Menu
                         </span>
+                        <MoreHorizontal className="h-4 w-4" />
                     </Button>
                 </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>
+                        Action
+                    </DropdownMenuLabel>
+                    <DropdownMenuItem>
+                        <Copy className="mr-2 h-4 w-4"/>
+                        Copy Id
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Edit className="mr-2 h-4 w-4"/>
+                        Update Id
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Delete className="mr-2 h-4 w-4"/>
+                        Trash Id
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
             </DropdownMenu>
         </div>
     );
