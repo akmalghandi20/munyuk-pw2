2c2689d48262010ff21901eec7a3d0fbce909904
DayanuArrofi <dayanuarrofi@gmail.com>
2025-01-25 22:41:32 +0700

    dayanu: membuat fungsi tombol update & delete

--- app/(dashboard)/[storeId]/(routes)/banners/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/components/cell-action.tsx
@@ -5,48 +5,72 @@ import { BannerColumn } from "./columns"
 import { Button } from "@/components/ui/button"
 import { Copy, Delete, Edit, MoreHorizontal } from "lucide-react"
 import toast from "react-hot-toast"
+import { useParams, useRouter } from "next/navigation"
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-    data: BannerColumn
+  data: BannerColumn;
 }
 
-export const CellAction: React.FC<CellActionProps> = ({
-    data
-}) => {
-    const onCopy = (id: String ) => {
-        navigator.clipboard.writeText(id);
-        toast.success("Banner Id berhasil di copy");
-    }
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
 
-    return (
-        <div>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost">
-                        <span className="sr-only">
-                            Open Menu
-                        </span>
-                        <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>
-                        Action
-                    </DropdownMenuLabel>
-                    <DropdownMenuItem  onClick={() => onCopy(data.id)}>
-                        <Copy className="mr-2 h-4 w-4"/>
-                        Copy Id
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Edit className="mr-2 h-4 w-4"/>
-                        Update Id
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Delete className="mr-2 h-4 w-4"/>
-                        Trash Id
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
-    );
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Banner Id berhasil di copy");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(/api/${params.storeId}/banners/${data.id});
+      router.refresh();
+      router.push(/${params.storeId}/banners);
+      toast.success("Banner berhasil dihapus");
+    } catch (error) {
+      toast.error("Cek kembali data dan koneksi mu");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+    <AlertModal 
+    isOpen={open}
+    onClose={() => setOpen(false)}
+    onConfirm={onDelete}
+    loading={loading}
+    />
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <span className="sr-only">Open Menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => onCopy(data.id)}>
+          <Copy className="mr-2 h-4 w-4" />
+          Copy Id
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => router.push(/${params.storeId}/banners/${data.id})}
+        >
+          <Edit className="mr-2 h-4 w-4" />
+          Update
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setOpen(true)}>
+          <Trash className="mr-2 h-4 w-4" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+    </>
+  );
 };
